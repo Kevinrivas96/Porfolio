80a5999135074aa2eff9ab448e24833a5993e24f
Kevinrivas96 <99252792+Kevinrivas96@users.noreply.github.com>
2024-02-28 17:07:51 -0500

    second version

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const codeBks = document.querySelector("#codeBks");
+
+
+// Remove Bouncing Effect after 2s from the icon to avoid distraction.
+setTimeout(() => {
+    codeBks.classList.remove("fa-bounce");
+}, 2000);
